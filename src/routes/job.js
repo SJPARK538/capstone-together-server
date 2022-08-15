1989e40b2ea79f2289a03f770cd4464ac3e407c6
sjpark538 <phi5388@gmail.com>
2022-08-14 23:18:38 -0400

    update user route methods

--- src/routes/job.js
+++ src/routes/job.js
@@ -4,37 +4,36 @@ const prisma = new PrismaClient();
 const fs = require('fs');
 const { userInfo } = require('os');
 
-
-router.post("/", async(req, res)=>{
-    const samplejob1 = {
-        title: "find a delivery person",
-        category: "delivery",
-        region: "North York",
-        payType: "Flat",
-        payRate: "25",
-        postDate: new Date().toISOString(),
-        dueDate: "Aug 27, 2022",
-        user_id: 1,
-        include: {
-            author: true
-        }
-    }
-    const test = await prisma.job.create({
-        data: {
-            'title': samplejob1.title,
-            'category': samplejob1.category,
-            'region': samplejob1.region, 
-            'payType': samplejob1.payType,
-            'payRate': samplejob1.payRate,
-            'postDate':samplejob1.postDate,
-            'dueDate':samplejob1.dueDate,
-            'user_id':samplejob1.user_id,
-            'author': samplejob1.author
-        }
-    })  
-    res.send(test)
-    console.log(test)
-})
+// CREATE A SAMPLE JOB LIST 
+// router.post("/", async(req, res)=>{
+//     const samplejob1 = {
+//         title: "find a delivery person",
+//         category: "delivery",
+//         region: "North York",
+//         payType: "Flat",
+//         payRate: "25",
+//         postDate: new Date().toISOString(),
+//         dueDate: "Aug 27, 2022",
+//         user_id: 1,
+//         include: {
+//             author: true
+//         }
+//     }
+//     const test = await prisma.job.create({
+//         data: {
+//             'title': samplejob1.title,
+//             'category': samplejob1.category,
+//             'region': samplejob1.region, 
+//             'payType': samplejob1.payType,
+//             'payRate': samplejob1.payRate,
+//             'postDate':samplejob1.postDate,
+//             'dueDate':samplejob1.dueDate,
+//             'user_id':samplejob1.user_id,
+//             'author': samplejob1.author
+//         }
+//     })  
+//     res.send(test)
+// })
 
 
 // Get all job lists
@@ -58,7 +57,23 @@ router.post("/", async(req, res)=>{
 
 // })
 
-
+// CREATE A JOB 
+router.post("/", async(req,res)=>{
+    const {title, category, region, payType, payRate, postDate, dueDate} = req.body;
+    const job  = await prisma.job.create({
+        data: {
+                        title: title,
+                        category: category,
+                        region: region, 
+                        payType: payType,
+                        payRate:payRate,
+                        postDate: postDate,
+                        dueDate: dueDate,
+                        include: {author: true}
+        }
+    })
+    res.json(user);
+})
 
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,53 +3,84 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const fs = require('fs');
 
-// sign up
-router.post("/", async(req, res)=>{
-    const userinfo = {
-        email: "abc@test.com",
-        name: "sejin park",
-        password: "default"
-    }  
+// SIGN UP -CREATE A SAMPLE USER
+// router.post("/", async(req, res)=>{
+//     const userinfo = {
+//         email: "abc@test.com",
+//         name: "sejin park",
+//         password: "default"
+//     }  
 
-    const test = await prisma.user.create({
+//     const test = await prisma.user.create({
+//         data: {
+//             'email': userinfo.email,
+//             'name': userinfo.name,
+//             'password': userinfo.password
+
+//         }
+//     })  
+//     res.send(test)
+// })
+
+
+
+// CREATE A USER
+router.post("/", async(req,res)=>{
+    const {name, email, password} = req.body;
+    const user  = await prisma.user.create({
         data: {
-            'email': userinfo.email,
-            'name': userinfo.name,
-            'password': userinfo.password
-
+            name: name,
+            email: email,
+            password: password,
         }
-    })  
-    res.send(test)
-
+    })
+    res.json(user);
 })
 
-// get information
-
-// router.post("/", async(req,res)=>{
-//     const {name, email, password} = req.body;
-//     const user  = await prisma.user.create({
-//         data: {
-//             name: name,
-//             email: email,
-//             password: password,
-//         }
-//     })
-//     res.json(user);
-// })
-
-// // all users list
-// router.get("/", async(req, res)=>{
-//     const users = await prisma.user.findMany();
-//     res.json(users);
-// })
-
-// router.put("/", async(req, res)=>{
-    
-// })
 
 
-// router.delete("/", async(req, res)=>{
-    
-// })
+// GET USER LIST
+router.get("/", async(req, res)=>{
+    const users = await prisma.user.findMany();
+    res.json(users);
+})
+
+// GET A SIGNEL USER INFORMATOIN BY ID
+router.get("/:id", async(req, res)=>{
+    const id = req.params.id;
+    const users = await prisma.user.findUnique({
+        where: {
+            id: id,
+        }
+    })
+})
+
+
+// UPDATE USER INFORMATION
+router.put("/", async(req, res)=>{
+    const {id, job, email} = req.body
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: id
+        },
+        data: {
+            job: job,
+            email: email
+        }
+    })
+    res.json(updatedUser);
+})
+
+
+router.delete("/:id", async(req, res)=>{
+    const id = req.params.id
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: id,
+
+        }
+    })
+    res.json(deletedUser)
+})
 
 module.exports = router;
